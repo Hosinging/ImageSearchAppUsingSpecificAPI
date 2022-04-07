942587a542545f3024b04b9ccf8be91c75b75198
Theo <>
2022-04-07 13:23:42 +0900

    feat: 프로젝트 초기화 및 사용할 패키지 추가

---
+++ ImageSearchAppUsingSpecificAPI/ImageSearchAppUsingSpecificAPI/ImageSearchAppUsingSpecificAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchAppUsingSpecificAPIApp.swift
+//  ImageSearchAppUsingSpecificAPI
+//
+//  Created by Theo on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearchAppUsingSpecificAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
