90a395a8f8be1263c77f734c0b780f6621de27fd
Theo <>
2022-04-08 02:42:25 +0900

    feat: DetailView재사용 가능하도록 뷰 익스텐션으로 분리 및 이미지뷰 하단에 정보 띄우도록 구현

--- ImageSearchAppUsingSpecificAPI/ImageSearchAppUsingSpecificAPI/View/DetailView.swift
+++ ImageSearchAppUsingSpecificAPI/ImageSearchAppUsingSpecificAPI/View/DetailView.swift
@@ -12,14 +12,31 @@ struct DetailView: View {
     let document: Document
     
     var body: some View {
+        CustomImageView
+    }
+}
+
+extension DetailView {
+    private var CustomImageView: some View {
         ScrollView([.vertical]) {
                 KFImage(URL(string: document.imageUrl))
                     .resizable()
                     .edgesIgnoringSafeArea(.all)
+                    .overlay(alignment: .bottom, content: {
+                        if document.displaySiteName != nil, document.datetime != nil {
+                            VStack {
+                                Text(document.displaySiteName ?? "")
+                                Text(document.datetime?.adjustFormat() ?? "")
+                            }
+                            .font(.system(size: 20, weight: .bold))
+                            .padding()
+                            .foregroundColor(Color.green)
+                        }
+                    })
                     .aspectRatio(contentMode: .fill)
-                
         }
         .background(Color.black)
     }
+    
 }
 
